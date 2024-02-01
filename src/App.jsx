367afcbbb5dcbb19ac8bd0b383c819405f2ece64
SarunasT <sarunas.tarvydas@hotmail.com>
2024-02-01 21:46:58 +0100

    tailwind work

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import './styles/App.css';
 export default function App() {
   console.log('App susikure');
   return (
-    <div className='App container'>
-      <h1>Hello, world!</h1>
+    <div className="App container mx-auto">
+      <h1 className="text-3xl font-bold underline">Hello, world!</h1>
+      <button className="px-4 py-2 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm">Save Changes</button>
       <hr />
     </div>
   );
